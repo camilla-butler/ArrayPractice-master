8a23c4a942ee520b2a914cf9b466bbc9aed81597
camillabutler <camilla_butler25@milton.edu>
2023-02-21 11:29:27 -0500

    first commit

---
+++ src/Mild2.java
@@ -0,0 +1,30 @@
+/***
+ * Goal: Determine and print the mean of array "a"
+ * Rating: MILD
+ */
+
+public class Mild2 {
+
+    public int[] a;
+    public double mean; // double instead of int because mean might be a decimal
+
+    public static void main(String[] args) {
+        Mild2 practice = new Mild2();
+    }
+
+    public Mild2() {
+
+        a = new int[20];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int)(Math.random()*100);
+        }
+   //int mean =
+
+
+
+        System.out.println("The mean is " + mean);
+
+    }
+
+}
